d4057906f1e1ac5df454de4106da814f441ac6c8
zeina <catsdisownedz@gmail.com>
2024-05-01 00:17:26 +0300

    database test

--- src/main/java/triviagamee/DatabaseConnection.java
+++ src/main/java/triviagamee/DatabaseConnection.java
@@ -1,7 +1,11 @@
 package triviagamee;
 
-//import java.sql.*;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
 
 public class DatabaseConnection {
-
+    String connectionURL = "jdbc:sqlserver://etcidk";
+    public static void main(String[] args) {
+        
+    }
 }
