84c6dd8f11c5674d3bf3f3471cee0c18fdeb2f81
noah-mclain <nadam.30032415@gmail.com>
2024-05-05 05:22:31 +0300

    added the button and function to display the genre select as a popup in the waiting room

--- src/main/java/triviagamee/PlayerWaitingRoom.java
+++ src/main/java/triviagamee/PlayerWaitingRoom.java
@@ -1,13 +1,25 @@
 package triviagamee;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PlayerWaitingRoom {
     public Label waitingLabel;
     public TextArea userUpdate;
     public Label editableLabel;
+    @FXML
+    Button genrePopUp;
 
 
     public void chat(){
@@ -18,5 +30,14 @@ public class PlayerWaitingRoom {
                 editableLabel.setText("Typing...");
             }
         });
+    }
+
+    public void genrepopup(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("GenreSelectScreen.fxml"));
+        Stage popup = new Stage();
+        Scene scene = new Scene(root, 600, 400);
+        popup.setScene(scene);
+        popup.initModality(Modality.APPLICATION_MODAL);
+        popup.showAndWait();
     }
 }
