0111ecd57cc428f38c366afeeba8bf4ef10120eb
zeina <catsdisownedz@gmail.com>
2024-05-07 06:27:35 +0300

    Update HostOrJoinController.java

--- src/main/java/triviagamee/HostOrJoinController.java
+++ src/main/java/triviagamee/HostOrJoinController.java
@@ -172,7 +172,7 @@ public class HostOrJoinController {
             case clicked_once:
                 // Second click: Choose category (assuming text is entered)
                 if(gameNameHostTextField.getText().isEmpty()){
-                    buttonAudio("hehehe");
+                    buttonAudio("chipmunkhehe");
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setHeaderText("Nope sorry.");
                     alert.setContentText("Sorry mate you can't enter without a writing game name (ˉ▽￣～)\n That's a bit lazy...");
